3275c24876b300ab0116d38659bbe5597bd1314f
Camille_T <55944930+crankyfrog@users.noreply.github.com>
2025-03-11 19:06:34 +0100

    Add aside component (#1)

---
+++ components/aside/aside.tsx
@@ -0,0 +1,14 @@
+import "./aside.css"
+
+export default function Aside(){
+    return(
+        <aside>
+            <ul>
+                <li><a href="/animation-carousel">Animated carousel</a></li>
+                <li><a href="#">name</a></li>
+                <li><a href="#">name</a></li>
+                <li><a href="#">name</a></li>
+            </ul>
+        </aside>
+    )
+}
