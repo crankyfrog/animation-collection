cec1408c92ff3bdee6c57978929463351c4e6e62
crankyfrog <camille.tindogan@gmail.com>
2025-03-11 18:57:18 +0100

    Add aside component

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,18 @@ import styles from "./page.module.css";
 import LogoCarousel from "@/components/logo-carousel/logoCarousel";
 import Header from "@/components/header/header";
 import PageList from "@/components/pageList/pageList";
+import Aside from "@/components/aside/aside";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header/>
-      <PageList/>
+      <div className="main-area">
+        <Aside/>
+        <main>
+          <PageList/>
+        </main>
+      </div>
     </div>
   );
 }
