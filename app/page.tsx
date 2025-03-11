3b366af4e2257fef05b5829f311888a36f964ad8
crankyfrog <camille.tindogan@gmail.com>
2025-03-11 19:10:02 +0100

    vercel fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import LogoCarousel from "@/components/logo-carousel/logoCarousel";
 import Header from "@/components/header/header";
 import PageList from "@/components/pageList/pageList";
 import Aside from "@/components/aside/aside";
